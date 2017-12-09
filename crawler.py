d28c2965805d97d0be081a81c396f3a96dcf296a
Henrique Braga <henrique.inftec@hotmail.com>
2017-12-09 03:42:45 -0200

    Create crawler for bitcoin information

---
+++ crawler.py
@@ -0,0 +1,64 @@
+import aiohttp
+import asyncio
+import backoff
+import uvloop
+import datetime
+import async_timeout
+
+from time import time
+
+BASE_URL = 'https://www.mercadobitcoin.net/api'
+RESOURCE = 'BTC/day-summary/{day}'
+URL = '/'.join([BASE_URL,RESOURCE])
+START = datetime.date(2016, 1, 1)
+END = datetime.date(2017, 1, 1)
+
+TIMEOUT_IN_SECONDS = 10
+
+
+@backoff.on_exception(backoff.expo, asyncio.TimeoutError, max_tries=8)
+async def get_daily_balance(url, session):
+    with async_timeout.timeout(TIMEOUT_IN_SECONDS):
+        async with session.get(url) as response:
+            print(await response.text())
+
+
+async def get_year_balance(from_year):
+    urls = _create_urls(start_year=from_year)
+
+    async with aiohttp.ClientSession() as session:
+
+        tasks = [
+            asyncio.ensure_future(get_daily_balance(url, session))
+            for url in urls
+        ]
+
+        return await asyncio.gather(*tasks)
+
+
+def _create_urls(start_year):
+    urls = []
+    
+    start = datetime.date(start_year, 1, 1)
+    end  = datetime.date(start_year + 1, 1, 1)
+
+    while start < end:
+        urls.append( 
+            URL.format(day=str(start).replace('-', '/'))
+        )
+        start = start + datetime.timedelta(days=1)
+
+    return urls
+
+
+if __name__ == '__main__':
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    loop = asyncio.get_event_loop()
+    start_time = time()
+    print('Starting executing script')
+    loop.run_until_complete(
+        get_year_balance(from_year=2016)
+    )
+    print('\n\nDone! It took {} seconds'.format(time() - start_time))
+    loop.close()
+    
